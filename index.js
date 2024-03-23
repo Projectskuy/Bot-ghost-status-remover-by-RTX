037860343660731d39439204f2512881c2903ed4
DenzzForTeams <96470753+Projectskuy@users.noreply.github.com>
2024-03-23 18:47:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening Denzz.apk","Watching Denzz.apk"];
 
 
 let currentIndex = 0;
